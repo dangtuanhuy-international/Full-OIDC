1b8a2f693b3a289fb22666ee7470430acb2a2a54
APAC\DAG8HC <dang8hc@bosch.com>
2023-02-23 13:59:56 +0700

    'init'

---
+++ CompanyEmployees.IDP/Config.cs
@@ -0,0 +1,81 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace CompanyEmployees.IDP;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> Ids =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+            new IdentityResources.Address(),
+            new IdentityResource("roles", "User role(s)", new List<string> { "role" }),
+            new IdentityResource("country", "Your country", new List<string> { "country" })
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+        {
+            new ApiScope("companyemployeeapi.scope", "CompanyEmployee API Scope")
+        };
+
+    public static IEnumerable<ApiResource> Apis =>
+        new ApiResource[]
+        {
+            new ApiResource("companyemployeeapi", "CompanyEmployee API")
+            {
+                    Scopes = { "companyemployeeapi.scope" }
+            }
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+        {
+            new Client
+            {
+                ClientName = "CompanyEmployeeClient",
+                ClientId = "companyemployeeclient",
+                AllowedGrantTypes = GrantTypes.Code,
+                RedirectUris = new List<string>{ "https://localhost:5010/signin-oidc" },
+                AllowedScopes = 
+                { 
+                    IdentityServerConstants.StandardScopes.OpenId, 
+                    IdentityServerConstants.StandardScopes.Profile, 
+                    IdentityServerConstants.StandardScopes.Address,
+                    "roles",
+                    "companyemployeeapi.scope",
+                    "country"
+                },
+                ClientSecrets = { new Secret("CompanyEmployeeClientSecret".Sha512()) },
+                RequirePkce = true,
+                RequireConsent = true,
+                PostLogoutRedirectUris = new List<string> { "https://localhost:5010/signout-callback-oidc" },
+                ClientUri = "https://localhost:5010",
+                AccessTokenLifetime = 120,
+                AllowOfflineAccess = true,
+                UpdateAccessTokenClaimsOnRefresh = true
+            },
+            new Client
+            {
+                    ClientName = "Angular-Client",
+                    ClientId = "angular-client",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = new List<string>{ "http://localhost:4200/signin-callback", "http://localhost:4200/assets/silent-callback.html" },
+                    RequirePkce = true,
+                    AllowAccessTokensViaBrowser = true,
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "companyApi"
+                    },
+                    AllowedCorsOrigins = { "http://localhost:4200" },
+                    RequireClientSecret = false,
+                    PostLogoutRedirectUris = new List<string> { "http://localhost:4200/signout-callback" },
+                    RequireConsent = false,
+                    AccessTokenLifetime = 600
+            }
+        };
+}
